836177730975279e8c6e2e3462feb692795fbc7b
Young Woo Choi <ywchoi.ys@gmail.com>
2021-02-13 00:52:58 +0900

    install script

--- install.sh
+++ install.sh
@@ -7,6 +7,16 @@ rsync -avzh ./gitconfig ~/.gitconfig
 rsync -avzh ./vimrc ~/.vimrc
 rsync -avzh ./vim/* ~/.vim
 
+# host-dependent configs
+HOSTNAME=$(hostname)
+if [[ "$HOSTNAME" == "ywchoi.local" ]]; then
+  rync -avzh ./zshrc.local ~/.zshrc.local
+elif [[ "$HOSTNAME" == "palm.yonsei.ac.kr" ]]; then
+  rync -avzh ./zshrc.palm ~/.zshrc.palm
+elif [[ "$HOSTNAME" = "login"* ]]; then
+  rync -avzh ./zshrc.nurion ~/.zshrc.nurion
+fi
+
 source ~/.zshrc;
 
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
