3592594aadf42b73d12ae902b66786ff4fcad8c0
Young Woo Choi <hi.ywchoi@gmail.com>
2021-06-24 20:01:49 -0700

    0624

---
+++ hosts/local/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+rsync -avzh hosts/local/zshrc ~/.zshrc
+rsync -avzh hosts/local/karabiner ~/.config/karabiner
--- install.sh
+++ install.sh
@@ -1,28 +1,53 @@
 #!/usr/bin/env zsh
 
-git pull origin master
+HOST=$1
+HOSTLIST=("local" "cori")
 
-rsync -avzh ./zshrc ~/.zshrc
-rsync -avzh ./gitconfig ~/.gitconfig
-rsync -avzh ./vimrc ~/.vimrc
-rsync -avzh ./vim/* ~/.vim
+main() {
 
-# host-dependent configs
-HOSTNAME=$(hostname)
-if [[ "$HOSTNAME" == "ywchoi.local" ]]; then
-  rsync -avzh ./zshrc.local ~/.zshrc.local
-elif [[ "$HOSTNAME" == "palm.yonsei.ac.kr" ]]; then
-  rsync -avzh ./zshrc.palm ~/.zshrc.palm
-elif [[ "$HOSTNAME" = "login"* ]]; then
-  rsync -avzh ./zshrc.nurion ~/.zshrc.nurion
-fi
+  if [ -z "$HOST" ];
+  then
+    print_usage
+    exit 1;
+  fi
 
-source ~/.zshrc;
+  if [[ ! "${HOSTLIST[@]}" =~ "${HOST}" ]]; then
+    print_usage
+    exit 2;
+  fi
 
-if [ ! -f ~/.vim/autoload/plug.vim ]; then
-  # vim-plug
-  echo "Installing vim-plug.."
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
+  echo "Start installing for host: $HOST"
 
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --bin
+
+  rsync -avzh ./common/z.sh ~/.z.sh
+  rsync -avzh ./common/p10k.zsh ~/.p10k.zsh
+  rsync -avzh ./common/vimrc ~/.vimrc
+  rsync -avzh ./common/vim ~/vim
+  rsync -avzh ./common/zshrc ~/.zshrc.common
+
+  if [ -f ./hosts/${HOST}/install.sh ]; then
+    ./hosts/${HOST}/install.sh
+  fi
+
+  # source ~/.zshrc;
+
+  # if [ ! -f ~/.vim/autoload/plug.vim ]; then
+  #   # vim-plug
+  #   echo "Installing vim-plug.."
+  #   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  #       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  # fi
+}
+
+print_usage() {
+  echo "[Usage] ./install.sh host"
+  echo ""
+  echo "host list"
+  echo "- local"
+  echo "- cori"
+}
+
+main
