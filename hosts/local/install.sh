b793ef3aa43ef2607ef7a6ab2e3f917158de1010
Young Woo Choi <ywchoi@berkeley.edu>
2021-07-04 14:53:26 -0700

    local

--- hosts/local/install.sh
+++ hosts/local/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 TOPDIR=$(git rev-parse --show-toplevel)
-BASEDIR=${TOPDIR}/hosts/cori
+BASEDIR=${TOPDIR}/hosts/local
 
 ln -sf ${BASEDIR}/zshrc ~/.zshrc
 ln -sf ${BASEDIR}/karabiner ~/.config/karabiner
