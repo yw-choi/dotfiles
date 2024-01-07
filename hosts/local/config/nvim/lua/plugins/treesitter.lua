ba96b402eb873e0ffd589277ecb391d6ecf4a670
Young Woo Choi <ywchoi@berkeley.edu>
2024-01-07 14:26:34 -0800

    lazynvim, git aliases on local

---
+++ hosts/local/config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "fortran",
+        "python",
+      })
+    end,
+  },
+}
