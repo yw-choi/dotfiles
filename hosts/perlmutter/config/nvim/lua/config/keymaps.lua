591d41344a108904841b3bf3b3cb45ade2c05b52
Young Woo Choi <ywchoi@berkeley.edu>
2024-02-01 01:35:42 -0800

    perlmutter nvim

--- hosts/perlmutter/config/nvim/lua/config/keymaps.lua
+++ hosts/perlmutter/config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- map control+x to delete current buffer
+vim.api.nvim_set_keymap("n", "<C-x>", ":bd<CR>", { noremap = true, silent = true })
+
+-- normal mode tab to switch buffers
+vim.api.nvim_set_keymap("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = true })
+
+-- 0 to _ in normal mode
+vim.api.nvim_set_keymap("n", "0", "_", { noremap = true, silent = true })
--- hosts/perlmutter/config/nvim/lua/config/options.lua
+++ hosts/perlmutter/config/nvim/lua/config/options.lua
@@ -1,6 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.swapfile = false
+vim.wo.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.swapfile = false
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.g.fortran_free_source = 1
+vim.g.fortran_do_enddo = 1
